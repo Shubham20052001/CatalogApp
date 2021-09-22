432576dac6cf9f12db04d5c38a04b95bdc2a83fc
Shubham20052001 <77091152+Shubham20052001@users.noreply.github.com>
2021-09-22 17:00:54 +0530

    changed from listview to gridview.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
-import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -39,11 +38,35 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ? ListView.builder(
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items![index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(item.name,
+                              style: const TextStyle(color: Colors.white)),
+                          padding: const EdgeInsets.all(12),
+                          decoration:
+                              const BoxDecoration(color: Colors.deepPurple),
+                        ),
+                        child: Image.network(item.image),
+                        footer: Container(
+                          child: Text(item.price.toString(),
+                              style: const TextStyle(color: Colors.white)),
+                          padding: const EdgeInsets.all(12),
+                          decoration: const BoxDecoration(color: Colors.black),
+                        ),
+                      ));
+                },
                 itemCount: CatalogModel.items!.length,
-                itemBuilder: (context, index) => ItemWidget(
-                  item: CatalogModel.items![index],
-                ),
               )
             : const Center(
                 child: CircularProgressIndicator(),
